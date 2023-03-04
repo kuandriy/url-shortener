5bceac210b4a609338956a9745fd4853ab40ed16
Andriy Kudelya <kuandriy@gmail.com>
2023-03-04 15:57:40 -0800

    trigger pipeline

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 export const handler = async =>{
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda')
+        body: JSON.stringify('Hello from lambda 1')
     }
 }
