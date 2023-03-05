11d4d67de259b749fdfd66038dbdf79752281c3a
Andriy Kudelya <kuandriy@gmail.com>
2023-03-05 13:00:55 -0800

    Read shor url from path

--- get-full-url.ts
+++ get-full-url.ts
@@ -12,7 +12,7 @@ export const handler: Handler = async (
 	const REGION = "us-east-1";
 	const TableName = "short-to-original-url";
 
-	return JSON.stringify(event.requestContext) ;
+	return JSON.stringify(event.requestContext.path) ;
 	const shortUrl = "event.pathParameters";
 
 	// Get original Url from db
