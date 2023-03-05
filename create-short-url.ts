33759aa4aac4d0c54e6fd3803cc06e7ba2244ecb
Andriy Kudelya <kuandriy@gmail.com>
2023-03-04 17:43:16 -0800

    Adding types

--- create-short-url.ts
+++ create-short-url.ts
@@ -1,6 +1,9 @@
-export const handler = async =>{
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from lambda 2')
-    }
-}
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
+
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+  const response = {
+    statusCode: 200,
+    body: `Test Create Short Url`,
+  };
+  return response;
+};
