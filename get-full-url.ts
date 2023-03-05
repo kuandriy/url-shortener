554f4968c8e9d9792c42873c82fde96c037a7de8
Andriy Kudelya <kuandriy@gmail.com>
2023-03-04 19:59:21 -0800

    fix key

--- get-full-url.ts
+++ get-full-url.ts
@@ -15,9 +15,9 @@ export const handler: Handler = async (
 	const getItemParams = {
 		TableName,
 		Key: {
-			partition_key: { S: 'test' }
+            partitionKey: { S: "test" },
 		},
-		ProjectionExpression: 'originalurl'
+		ProjectionExpression: "originalurl"
 	};
 
 	const dynamodbClient = new DynamoDBClient({ region: REGION });
