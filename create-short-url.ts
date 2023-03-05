b6828d39c22ac10a4905ff3a8629ca96d88fd4a9
Andriy Kudelya <kuandriy@gmail.com>
2023-03-05 11:17:05 -0800

    Hash support

--- create-short-url.ts
+++ create-short-url.ts
@@ -4,20 +4,51 @@ import {
 	Handler
 } from "aws-lambda";
 
-import { DynamoDBClient, ListTablesCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
+const crypto = require("crypto");
 
 export const handler: Handler = async (
 	event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResultV2> => {
-	const dbClient = new DynamoDBClient({ region: "us-west-2" });
-	const command = new ListTablesCommand({});
+	const REGION = "us-east-1";
+	const TableName = "urlshortener";
 
-	try {
-		const results = await dbClient.send(command);
-		console.log(results.TableNames.join("\n"));
-	} catch (err) {
-		console.error(err);
+	if (!event.body) {
+		return "Url is required";
 	}
 
-	return "";
+	const data = JSON.parse(event.body);
+
+	if (!data.url) {
+		return "Url is required";
+	}
+
+	const url = data.url;
+
+	// Using url hash to query DB, to find if record already exist
+	const urlHash = crypto.createHash("md5").update(url).digest("hex");
+
+	//DB object
+	const dynamodbClient = new DynamoDBClient({ region: REGION });
+
+	// Check if we  have short url for received url
+	const getItemByHashParams = {
+		TableName,
+		Key: {
+			urlhash: { S: urlHash }
+		},
+		ProjectionExpression: "short"
+	};
+
+	const getItemCommand = new GetItemCommand(getItemByHashParams);
+
+	try {
+		const dbResult = await dynamodbClient.send(getItemCommand);
+		if (dbResult.Item) {
+			return dbResult.Item.short.S.concat("     ", urlHash);
+		}
+	} catch (err) {
+		console.log(err);
+		return err;
+	}
 };
