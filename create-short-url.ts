3b91b6ec3ed6de91b58f57c29f25cbf8fb9cd4e0
Andriy Kudelya <kuandriy@gmail.com>
2023-03-05 14:46:35 -0800

    Adding Index fot hash url

--- create-short-url.ts
+++ create-short-url.ts
@@ -51,7 +51,7 @@ export const handler: Handler = async (
 		if (!getItemResult) {
 			return "not found".concat(hashUrl);
 		}
-        return getItemResult.shorturl;
+        return getItemResult;
 	} catch (err) {
 		console.log(err);
 		return err;
